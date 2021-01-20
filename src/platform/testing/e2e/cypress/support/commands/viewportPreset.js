7bd1fc95cb56a9e62bdf31e1fa49b60f7acabd5c
Holden Hinkle <holden@holdenhinkle.com>
2021-01-20 18:10:59 -0500

    update viewportPreset.js

--- src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
+++ src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
@@ -1,16 +1,19 @@
 const presets = {
   // Top mobile presets by traffic percentage, descending
   'va-top-mobile-1': { width: 414, height: 896 },
-  'va-top-mobile-2': { width: 375, height: 667 },
-  'va-top-mobile-3': { width: 375, height: 812 },
+  'va-top-mobile-2': { width: 375, height: 812 },
+  'va-top-mobile-3': { width: 375, height: 667 },
   'va-top-mobile-4': { width: 414, height: 736 },
-  'va-top-mobile-5': { width: 320, height: 568 },
+  'va-top-mobile-5': { width: 360, height: 640 },
+  'va-top-mobile-6': { width: 320, height: 568 },
+  'va-top-mobile-7': { width: 412, height: 869 },
+  'va-top-mobile-8': { width: 412, height: 846 },
+  'va-top-mobile-9': { width: 360, height: 740 },
+  'va-top-mobile-10': { width: 360, height: 760 },
   // Top tablet presets by traffic percentage, descending
   'va-top-tablet-1': { width: 768, height: 1024 },
-  'va-top-tablet-2': { width: 1080, height: 1920 },
-  'va-top-tablet-3': { width: 720, height: 1280 },
-  'va-top-tablet-4': { width: 800, height: 1280 },
-  'va-top-tablet-5': { width: 601, height: 962 },
+  'va-top-tablet-2': { width: 1920, height: 1080 },
+  'va-top-tablet-3': { width: 1280, height: 720 },
   // Top desktop presets by traffic percentage, descending
   'va-top-desktop-1': { width: 1280, height: 960 },
   'va-top-desktop-2': { width: 1920, height: 1080 },
